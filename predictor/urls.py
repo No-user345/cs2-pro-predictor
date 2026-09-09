336abcb66bb6f80ebea8be69f86bfb837ca43140
No-user345 <denisandsketch552@gmail.com>
2026-09-09 21:54:16 +0100

    added static url

--- predictor/urls.py
+++ predictor/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url="/static/favicon.png", permanent=True)
+    ),
+
     path('', views.fantasy),
     path("fantasy-results/", views.fantasy_results),
 ]
